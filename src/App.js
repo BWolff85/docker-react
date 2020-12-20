7bb97cfa134d60920542fdc018de4bf3f5b37944
BWolff85 <brittzyx@msn.com>
2020-12-20 14:51:14 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Khumbu and Thelma are silly
         </p>
         <a
           className="App-link"
